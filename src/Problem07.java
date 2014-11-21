b0a2a3ed6fc1705cb8a763d1c5c8e15cdf8c2549
Aleksander Grzybowski <kelog@orange.pl>
2014-11-21 19:38:54 +0100

    Better isPrime on streams

--- src/Problem07.java
+++ src/Problem07.java
@@ -4,13 +4,7 @@ import java.util.stream.IntStream;
 public class Problem07 {
 
 	public static boolean isPrime(int number) {
-		if (number == 1) return false;
-
-		for (int i = 2; i < number; ++i)
-			if (number % i == 0)
-				return false;
-
-		return true;
+		return (number != 1) && IntStream.range(2, number).parallel().noneMatch(i -> number % i == 0);
 	}
 
 	public static void main(String[] args) {
