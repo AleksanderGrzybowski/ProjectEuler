1da2ed77b3bd4ce01bf9b751642439d85f7864d9
Aleksander Grzybowski <kelog@orange.pl>
2014-11-28 10:21:47 +0100

    Problem 15

---
+++ src/Problem15.java
@@ -0,0 +1,28 @@
+@Done
+public class Problem15 implements Problem<Long> {
+
+	static final int SIZE = 20;
+	static long[][] tab = new long[SIZE + 1][SIZE + 1];
+
+	@Override
+	public Long getCalculatedSolution() {
+		for (int i = 0; i <= SIZE; ++i) {
+			tab[SIZE][i] = 1;
+			tab[i][SIZE] = 1;
+		}
+
+		for (int row = SIZE -1; row >= 0; --row) {
+			for (int col = SIZE -1; col >= 0; --col) {
+				tab[row][col] = tab[row+1][col] + tab[row][col+1];
+			}
+		}
+
+		return tab[0][0];
+	}
+
+	@Override
+	public Long getExpectedSolution() {
+		return 137846528820L;
+	}
+}
+
