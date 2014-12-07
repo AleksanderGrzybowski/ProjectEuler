fc37bb5857e5bb81b4bba10e5b971b7cfe8f1d8c
Aleksander Grzybowski <kelog@orange.pl>
2014-12-07 21:44:12 +0100

    Problem 5 faster

--- src/problems/Problem05.java
+++ src/problems/Problem05.java
@@ -2,9 +2,6 @@ package problems;
 
 import annotations.Done;
 
-import java.util.function.IntPredicate;
-import java.util.stream.IntStream;
-
 @Done
 
 public class Problem05 implements Problem<Integer> {
@@ -12,21 +9,33 @@ public class Problem05 implements Problem<Integer> {
 	static final int FIRST_DIVISOR = 1;
 	static final int LAST_DIVISOR = 20;
 
-	public IntPredicate getModPredicate(int number) {
-		return x -> (x % number == 0);
-	}
-
-	public IntStream getFilteredStream(IntStream stream) {
-		for (int i = FIRST_DIVISOR; i <= LAST_DIVISOR; ++i)
-			stream = stream.filter(getModPredicate(i));
-		return stream;
-	}
+	// the following solution is way more Java-8-ish, but slow...
+//	public IntPredicate getModPredicate(int number) {
+//		return x -> (x % number == 0);
+//	}
+//
+//	public IntStream getFilteredStream(IntStream stream) {
+//		for (int i = FIRST_DIVISOR; i <= LAST_DIVISOR; ++i)
+//			stream = stream.filter(getModPredicate(i));
+//		return stream;
+//	}
+//
+//		@Override
+//	public Integer getCalculatedSolution() {
+//		IntStream naturalNumbers = IntStream.iterate(1, i -> i + 1);
+//
+//		return getFilteredStream(naturalNumbers).findFirst().getAsInt();
+//	}
 
 	@Override
 	public Integer getCalculatedSolution() {
-		IntStream naturalNumbers = IntStream.iterate(1, i -> i + 1);
-
-		return getFilteredStream(naturalNumbers).findFirst().getAsInt();
+		outer:
+		for (int number = 1; true; number++) {
+			for (int divisor = FIRST_DIVISOR; divisor <= LAST_DIVISOR; ++divisor)
+				if (number % divisor != 0)
+					continue outer;
+			return number;
+		}
 	}
 
 	@Override
