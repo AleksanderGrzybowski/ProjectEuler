de1e30121b3fcb23db17c9de469205bf21f80d6d
Aleksander Grzybowski <kelog@orange.pl>
2015-02-14 15:40:37 +0100

    44

---
+++ src/problems/Problem44.java
@@ -0,0 +1,34 @@
+package problems;
+
+import annotations.Done;
+
+import java.util.Set;
+import java.util.function.IntUnaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Done
+public class Problem44 implements Problem<Integer> {
+
+	@Override
+	public Integer getCalculatedSolution() {
+		IntUnaryOperator pentFormula = n -> (n * (3 * n - 1)) / 2;
+		Set<Integer> set = IntStream.rangeClosed(1, 10000).map(pentFormula).boxed().collect(Collectors.toSet());
+
+		int d = Integer.MAX_VALUE;
+
+		for (int pj : set) {
+			for (int pk : set) {
+				if (set.contains(pj + pk) && set.contains(Math.abs(pj - pk))) {
+					d = Math.min(d, Math.abs(pj - pk));
+				}
+			}
+		}
+		return d;
+	}
+
+	@Override
+	public Integer getExpectedSolution() {
+		return 5482660;
+	}
+}
