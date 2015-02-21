9a77d6f7c612ebabdc74a1ddec966d75d51263a9
Aleksander Grzybowski <kelog@orange.pl>
2015-02-21 15:51:45 +0100

    39

---
+++ src/problems/Problem39.java
@@ -0,0 +1,53 @@
+package problems;
+
+import annotations.Done;
+import utils.Triple;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Done
+public class Problem39 implements Problem<Integer> {
+
+	public static int numberOfSolutionsFor(int p) {
+		HashSet<Triple> triangles = new HashSet<>();
+
+		for (int a = 1; a <= (p - 2); ++a) {
+			for (int b = 1; b <= (p - 2); ++b) {
+
+				int c = p - b - a;
+				// naive, but works
+				if ((a + b + c != p) || c <= 0)
+					continue;
+
+				if (a * a + b * b == c * c) {
+					int[] sides = new int[]{a, b, c};
+					Arrays.sort(sides);
+					triangles.add(new Triple(sides[0], sides[1], sides[2]));
+				}
+			}
+		}
+		return triangles.size();
+	}
+
+	@Override
+	public Integer getCalculatedSolution() {
+		int currentMaxP = 0;
+		int currentMaxSols = 0;
+
+		for (int p = 3; p <= 1000; ++p) {
+			int nSols = numberOfSolutionsFor(p);
+
+			if (currentMaxSols < nSols) {
+				currentMaxP = p;
+				currentMaxSols = nSols;
+			}
+		}
+		return currentMaxP;
+	}
+
+	@Override
+	public Integer getExpectedSolution() {
+		return 840;
+	}
+}
