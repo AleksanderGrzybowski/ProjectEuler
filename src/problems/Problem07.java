98748a8fe73a666cd48a60247893aaa5aedb71fa
Aleksander Grzybowski <kelog@orange.pl>
2015-02-21 22:02:02 +0100

    07 remove stupid looping through all numbers

--- src/problems/Problem07.java
+++ src/problems/Problem07.java
@@ -3,20 +3,18 @@ package problems;
 import annotations.Done;
 import utils.PrimeCache;
 
-import java.util.stream.IntStream;
+import java.util.ArrayList;
 
 @Done
-
 public class Problem07 implements Problem<Integer> {
-
-	PrimeCache pc = new PrimeCache(200_000);
 
 	@Override
 	public Integer getCalculatedSolution() {
-		IntStream naturalNumbers = IntStream.iterate(1, i -> i + 1);
-		int index = 10_001;
+		final int LIMIT = 200_000;
+		PrimeCache cache = new PrimeCache(LIMIT);
+		final int INDEX = 10_001;
 
-		return naturalNumbers.filter(pc::isPrime).skip(index - 1).findFirst().getAsInt();
+		return new ArrayList<>(cache.asSortedSet()).get(INDEX - 1);
 	}
 
 	@Override
