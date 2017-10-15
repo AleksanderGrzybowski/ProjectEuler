2b06f2125cf1697f16d0ac44f6519c94b13b4d9c
Aleksander Grzybowski <kelog@orange.pl>
2017-10-15 15:14:39 +0200

    2

--- src/main/java/projecteuler/problems/Problem02.java
+++ src/main/java/projecteuler/problems/Problem02.java
@@ -1,10 +1,38 @@
 package projecteuler.problems;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.IntPredicate;
+
 public class Problem02 implements Problem<Integer> {
+    
+    private static final IntPredicate IS_EVEN = i -> i % 2 == 0;
     
     @Override
     public Integer getCalculatedSolution() {
-        return null;
+        return fibonacciUpTo(4_000_000).stream()
+                .mapToInt(e -> e)
+                .filter(IS_EVEN)
+                .sum();
+    }
+    
+    private List<Integer> fibonacciUpTo(int limit) {
+        List<Integer> elements = new ArrayList<>();
+        
+        int a = 1;
+        int b = 2;
+        
+        elements.add(a);
+        elements.add(b);
+        
+        while (b <= limit) {
+            int temp = b;
+            b = a + b;
+            a = temp;
+            elements.add(b);
+        }
+        
+        return elements;
     }
     
     @Override
