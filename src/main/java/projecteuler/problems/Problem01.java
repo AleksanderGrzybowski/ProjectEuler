69f520fbe52db51e342c64c0ab7ed247298e06e8
Aleksander Grzybowski <kelog@orange.pl>
2017-10-15 15:01:19 +0200

    1

--- src/main/java/projecteuler/problems/Problem01.java
+++ src/main/java/projecteuler/problems/Problem01.java
@@ -1,10 +1,19 @@
 package projecteuler.problems;
 
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
 public class Problem01 implements Problem<Integer> {
     
     @Override
     public Integer getCalculatedSolution() {
-        return null;
+        return IntStream.range(1, 1000)
+                .filter(isMultipleOf(3).or(isMultipleOf(5)))
+                .sum();
+    }
+    
+    private IntPredicate isMultipleOf(int factor) {
+        return number -> number % factor == 0;
     }
     
     @Override
