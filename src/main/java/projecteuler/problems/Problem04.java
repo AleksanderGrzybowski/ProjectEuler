12e576b9720d576fa01a0eb88178df42c8a0e1a7
Aleksander Grzybowski <kelog@orange.pl>
2017-10-15 16:20:55 +0200

    4

--- src/main/java/projecteuler/problems/Problem04.java
+++ src/main/java/projecteuler/problems/Problem04.java
@@ -1,10 +1,27 @@
 package projecteuler.problems;
 
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
+@SuppressWarnings("ConstantConditions")
 public class Problem04 implements Problem<Integer> {
+    
+    private static final IntPredicate IS_PALINDROMIC = i -> {
+        String numberString = "" + i;
+        return numberString.equals(new StringBuilder(numberString).reverse().toString());
+    };
     
     @Override
     public Integer getCalculatedSolution() {
-        return null;
+        return products()
+                .filter(IS_PALINDROMIC)
+                .max().getAsInt();
+    }
+    
+    private IntStream products() {
+        return IntStream.rangeClosed(100, 999).flatMap(
+                first -> IntStream.rangeClosed(100, 999).map(second -> first * second)
+        );
     }
     
     @Override
