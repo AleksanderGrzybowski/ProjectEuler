88cbf2331e9de8fe12e823c1cfc4890bf810033a
Aleksander Grzybowski <kelog@orange.pl>
2017-10-15 16:04:06 +0200

    3

--- src/main/java/projecteuler/problems/Problem03.java
+++ src/main/java/projecteuler/problems/Problem03.java
@@ -1,14 +1,45 @@
 package projecteuler.problems;
 
-public class Problem03 implements Problem<Long> {
-	
-	@Override
-	public Long getCalculatedSolution() {
-return null;
-	}
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 
-	@Override
-	public Long getExpectedSolution() {
-		return 6857L;
-	}
+@SuppressWarnings("ConstantConditions")
+public class Problem03 implements Problem<Integer> {
+    
+    @Override
+    public Integer getCalculatedSolution() {
+        return factorize(600851475143L).stream()
+                .max(Integer::compareTo)
+                .get();
+    }
+    
+    private List<Integer> factorize(long number) {
+        List<Integer> factors = new ArrayList<>();
+        
+        while (number != 1) {
+            int factor = smallestPrimeFactorOf(number);
+            factors.add(factor);
+            number /= factor;
+        }
+        
+        return factors;
+    }
+    
+    private static int smallestPrimeFactorOf(long value) {
+        return primes().filter(i -> value % i == 0).findFirst().getAsInt();
+    }
+    
+    private static IntStream primes() {
+        return naturalNumbers().filter(i -> IntStream.range(2, i).allMatch(e -> i % e != 0));
+    }
+    
+    private static IntStream naturalNumbers() {
+        return IntStream.iterate(2, i -> i + 1);
+    }
+    
+    @Override
+    public Integer getExpectedSolution() {
+        return 6857;
+    }
 }
