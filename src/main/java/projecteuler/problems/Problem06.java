00ad6d614ffe947aec4d341642fe88db12b80d81
Aleksander Grzybowski <kelog@orange.pl>
2017-10-16 17:51:28 +0200

    6

--- src/main/java/projecteuler/problems/Problem06.java
+++ src/main/java/projecteuler/problems/Problem06.java
@@ -1,14 +1,24 @@
 package projecteuler.problems;
 
+import java.util.stream.IntStream;
+
 public class Problem06 implements Problem<Integer> {
-
-	@Override
-	public Integer getCalculatedSolution() {
-		return null;
-	}
-
-	@Override
-	public Integer getExpectedSolution() {
-		return 25164150;
-	}
+    
+    @Override
+    public Integer getCalculatedSolution() {
+        return square(range().sum()) - range().map(this::square).sum();
+    }
+    
+    private IntStream range() {
+        return IntStream.rangeClosed(1, 100);
+    }
+    
+    private int square(int number) {
+        return number * number;
+    }
+    
+    @Override
+    public Integer getExpectedSolution() {
+        return 25164150;
+    }
 }
