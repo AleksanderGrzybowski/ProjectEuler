e7603b78da58ddbce5a79d48b5665128d0150409
Aleksander Grzybowski <kelog@orange.pl>
2017-10-16 17:46:23 +0200

    5

--- src/main/java/projecteuler/problems/Problem05.java
+++ src/main/java/projecteuler/problems/Problem05.java
@@ -1,10 +1,24 @@
 package projecteuler.problems;
 
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
+@SuppressWarnings("ConstantConditions")
 public class Problem05 implements Problem<Integer> {
+    
+    // functional madness
+    private static final IntPredicate IS_DIVISIBLE_1_THROUGH_20 = IntStream
+            .of(11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+            .mapToObj(divisor -> (IntPredicate) candidate -> candidate % divisor == 0)
+            .reduce(IntPredicate::and)
+            .get();
     
     @Override
     public Integer getCalculatedSolution() {
-        return null;
+        return IntStream.iterate(20, i -> i + 20) // divisible by 20, so let's go up by 20
+                .parallel()
+                .filter(IS_DIVISIBLE_1_THROUGH_20)
+                .findFirst().getAsInt();
     }
     
     @Override
