767cae44c04f66087e1379882b13c49be764165a
Aleksander Grzybowski <kelog@orange.pl>
2017-10-16 18:07:28 +0200

    Show timing info

--- src/main/java/projecteuler/runner/Runner.java
+++ src/main/java/projecteuler/runner/Runner.java
@@ -4,10 +4,15 @@ import projecteuler.problems.Problem;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
 
 public class Runner {
     
     private static final int MAX_NUMBER_OF_PROBLEMS = 99;
+    
+    public static void main(String[] args) {
+        getAllProblems().forEach(Runner::run);
+    }
     
     private static List<Problem> getAllProblems() {
         List<Problem> problems = new ArrayList<>();
@@ -23,24 +28,37 @@ public class Runner {
     }
     
     private static void run(Problem problem) {
-        
-        System.out.print("" + problem.getClass());
+        System.out.print(problem.getClass().getSimpleName());
         Object expected = problem.getExpectedSolution();
-        Object actual = problem.getCalculatedSolution();
+    
+        TimedResult result = time(problem::getCalculatedSolution);
+        Object actual = result.value;
         
         if (actual == null) {
             System.out.println("  IGNORED");
         } else if (expected.equals(actual)) {
-            System.out.println("  PASS");
+            System.out.printf("  PASS  %4d ms\n", result.timeMS);
         } else {
             System.out.println("  FAIL: expected = " + expected + ", actual = " + actual);
             System.exit(1);
         }
     }
     
-    public static void main(String[] args) {
-        System.out.println("Starting...");
+    private static TimedResult time(Supplier<Object> function) {
+        long start = System.currentTimeMillis();
+        Object result = function.get();
+        long end = System.currentTimeMillis();
         
-        getAllProblems().forEach(Runner::run);
+        return new TimedResult(result, end - start);
+    }
+    
+    private static class TimedResult {
+        public final Object value;
+        public final long timeMS;
+        
+        private TimedResult(Object value, long timeMS) {
+            this.value = value;
+            this.timeMS = timeMS;
+        }
     }
 }
