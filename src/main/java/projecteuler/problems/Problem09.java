46bc53a9890f700ccb647b94209fb192745ff693
Aleksander Grzybowski <kelog@orange.pl>
2017-10-19 20:23:50 +0200

    9

--- src/main/java/projecteuler/problems/Problem09.java
+++ src/main/java/projecteuler/problems/Problem09.java
@@ -1,14 +1,26 @@
 package projecteuler.problems;
 
 public class Problem09 implements Problem<Integer> {
-
-	@Override
-	public Integer getCalculatedSolution() {
-return null;
-	}
-
-	@Override
-	public Integer getExpectedSolution() {
-		return 31875000;
-	}
+    
+    private static final int SUM = 1000;
+    
+    @Override
+    public Integer getCalculatedSolution() {
+        for (int i = 1; i <= SUM; ++i) {
+            for (int j = i; j <= SUM; ++j) {
+                int k = SUM - j - i;
+                
+                if (i * i + j * j == k * k) {
+                    return i * j * k;
+                }
+            }
+        }
+        
+        throw new AssertionError();
+    }
+    
+    @Override
+    public Integer getExpectedSolution() {
+        return 31875000;
+    }
 }
