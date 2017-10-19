dd54ce3f680297886cb44f3772407e80e7649e18
Aleksander Grzybowski <kelog@orange.pl>
2017-10-19 20:51:05 +0200

    Optimize prime search

--- src/main/java/projecteuler/problems/Common.java
+++ src/main/java/projecteuler/problems/Common.java
@@ -4,7 +4,7 @@ import java.util.stream.IntStream;
 
 public class Common {
     public static IntStream primes() {
-        return naturalNumbers().filter(i -> IntStream.range(2, i).allMatch(e -> i % e != 0));
+        return naturalNumbers().filter(i -> IntStream.rangeClosed(2, (int) Math.sqrt(i)).allMatch(e -> i % e != 0));
     }
     
     private static IntStream naturalNumbers() {
