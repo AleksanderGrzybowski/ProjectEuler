7203543b486757954f60df501782bdda8a2ad6d5
Aleksander Grzybowski <kelog@orange.pl>
2017-10-22 11:13:20 +0200

    14

--- src/main/java/projecteuler/problems/Problem14.java
+++ src/main/java/projecteuler/problems/Problem14.java
@@ -1,14 +1,32 @@
 package projecteuler.problems;
 
-public class Problem14 implements Problem<Integer> {
+import java.util.Comparator;
+import java.util.stream.LongStream;
+
+@SuppressWarnings("ConstantConditions")
+public class Problem14 implements Problem<Long> {
     
     @Override
-    public Integer getCalculatedSolution() {
-        return null;
+    public Long getCalculatedSolution() {
+        return LongStream.range(2, 1_000_000).boxed().parallel()
+                .max(Comparator.comparing(this::collatzLength)).get();
+    }
+    
+    private int collatzLength(long start) {
+        int length = 0;
+        long current = start;
+        
+        while (current != 1) {
+            length++;
+            
+            current = (current % 2 == 0) ? (current / 2) : (3 * current + 1);
+        }
+        
+        return length + 1;
     }
     
     @Override
-    public Integer getExpectedSolution() {
-        return 837799;
+    public Long getExpectedSolution() {
+        return 837799L;
     }
 }
