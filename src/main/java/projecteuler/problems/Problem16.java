4c502e95aaa779c31b0209bc5fd99b0889f7dc74
Aleksander Grzybowski <kelog@orange.pl>
2017-10-22 11:30:51 +0200

    16

--- src/main/java/projecteuler/problems/Problem16.java
+++ src/main/java/projecteuler/problems/Problem16.java
@@ -1,14 +1,19 @@
 package projecteuler.problems;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+
 public class Problem16 implements Problem<Integer> {
-
-	@Override
-	public Integer getCalculatedSolution() {
-	    return null;
-	}
-
-	@Override
-	public Integer getExpectedSolution() {
-		return 1366;
-	}
+    
+    @Override
+    public Integer getCalculatedSolution() {
+        return Arrays.stream(BigDecimal.valueOf(2).pow(1000).toString().split(""))
+                .mapToInt(Integer::valueOf)
+                .sum();
+    }
+    
+    @Override
+    public Integer getExpectedSolution() {
+        return 1366;
+    }
 }
