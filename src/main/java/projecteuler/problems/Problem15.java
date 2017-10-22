98fdab8c554696a6282b0c7ba3d64d37d2224a85
Aleksander Grzybowski <kelog@orange.pl>
2017-10-22 11:28:04 +0200

    15

--- src/main/java/projecteuler/problems/Problem15.java
+++ src/main/java/projecteuler/problems/Problem15.java
@@ -2,9 +2,23 @@ package projecteuler.problems;
 
 public class Problem15 implements Problem<Long> {
     
+    private static final int SIZE = 20;
+    private static final long[][] matrix = new long[SIZE + 1][SIZE + 1];
+    
     @Override
     public Long getCalculatedSolution() {
-        return null;
+        for (int i = 0; i <= SIZE; ++i) {
+            matrix[0][i] = 1;
+            matrix[i][0] = 1;
+        }
+        
+        for (int row = 1; row <= SIZE; ++row) {
+            for (int col = 1; col <= SIZE; ++col) {
+                matrix[row][col] = matrix[row - 1][col] + matrix[row][col - 1];
+            }
+        }
+        
+        return matrix[SIZE][SIZE];
     }
     
     @Override
