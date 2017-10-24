623176265bf0089ab6f5169afd07f7c6e28f18cf
Aleksander Grzybowski <kelog@orange.pl>
2017-10-24 18:23:08 +0200

    20

--- src/main/java/projecteuler/problems/Problem20.java
+++ src/main/java/projecteuler/problems/Problem20.java
@@ -1,14 +1,26 @@
 package projecteuler.problems;
 
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Problem20 implements Problem<Integer> {
-
-	@Override
-	public Integer getCalculatedSolution() {
-return null;
-	}
-
-	@Override
-	public Integer getExpectedSolution() {
-		return 648;
-	}
+    
+    @Override
+    public Integer getCalculatedSolution() {
+        return Arrays.stream(
+                IntStream.rangeClosed(1, 100)
+                        .mapToObj(BigInteger::valueOf)
+                        .reduce(BigInteger.ONE, BigInteger::multiply)
+                        .toString().split("")
+        )
+                .mapToInt(Integer::parseInt)
+                .sum();
+        
+    }
+    
+    @Override
+    public Integer getExpectedSolution() {
+        return 648;
+    }
 }
