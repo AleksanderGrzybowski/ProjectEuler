d0fe95aeeafcd72ff0f073debc34a9b173ab44a6
Aleksander Grzybowski <kelog@orange.pl>
2017-10-25 18:37:53 +0200

    21

--- src/main/java/projecteuler/problems/Problem21.java
+++ src/main/java/projecteuler/problems/Problem21.java
@@ -1,10 +1,39 @@
 package projecteuler.problems;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
 public class Problem21 implements Problem<Integer> {
+    
+    private static final Function<Integer, Integer> D = memoized(Problem21::sumOfproperDivisors);
     
     @Override
     public Integer getCalculatedSolution() {
-        return null;
+        int sum = 0;
+        
+        for (int a = 0; a < 10000; a++) {
+            for (int b = 0; b < 10000; b++) {
+                if (a == b) break;
+                if (D.apply(a) == b && D.apply(b) == a) {
+                    sum += a + b;
+                }
+            }
+        }
+        
+        return sum;
+    }
+    
+    // Beautiful, even if Java. 
+    private static <K, V> Function<K, V> memoized(Function<K, V> function) {
+        Map<K, V> cache = new HashMap<>();
+        
+        return argument -> cache.computeIfAbsent(argument, function);
+    }
+    
+    private static int sumOfproperDivisors(int number) {
+        return IntStream.range(1, number).filter(i -> number % i == 0).sum();
     }
     
     @Override
