38107009a944fb21ca4e4623463cc952bf9ae37b
Aleksander Grzybowski <kelog@orange.pl>
2017-10-28 12:49:31 +0200

    25

--- src/main/java/projecteuler/problems/Problem25.java
+++ src/main/java/projecteuler/problems/Problem25.java
@@ -1,10 +1,48 @@
 package projecteuler.problems;
 
+import java.math.BigInteger;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+@SuppressWarnings("ConstantConditions")
 public class Problem25 implements Problem<Integer> {
     
     @Override
     public Integer getCalculatedSolution() {
-        return null;
+        return fibbonacci().filter(f -> f.value.toString().length() == 1000).findFirst().get().index;
+    }
+    
+    private Stream<WithIndex> fibbonacci() {
+        return Stream.generate(new Supplier<WithIndex>() {
+            
+            private BigInteger a = BigInteger.ONE;
+            private BigInteger b = BigInteger.ONE;
+            private int currentIndex = 0;
+            
+            @Override
+            public WithIndex get() {
+                currentIndex++;
+                
+                if (currentIndex == 1 || currentIndex == 2) {
+                    return new WithIndex(currentIndex, BigInteger.ONE);
+                } else {
+                    BigInteger result = a.add(b);
+                    a = b;
+                    b = result;
+                    return new WithIndex(currentIndex, result);
+                }
+            }
+        });
+    }
+    
+    private static class WithIndex {
+        final int index;
+        final BigInteger value;
+        
+        WithIndex(int index, BigInteger value) {
+            this.index = index;
+            this.value = value;
+        }
     }
     
     @Override
