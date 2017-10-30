07592e512f4e049437d7087f54927a39148463a5
Aleksander Grzybowski <kelog@orange.pl>
2017-10-30 18:11:02 +0100

    27

--- src/main/java/projecteuler/problems/Common.java
+++ src/main/java/projecteuler/problems/Common.java
@@ -4,7 +4,11 @@ import java.util.stream.IntStream;
 
 public class Common {
     public static IntStream primes() {
-        return naturalNumbers().filter(i -> IntStream.rangeClosed(2, (int) Math.sqrt(i)).allMatch(e -> i % e != 0));
+        return naturalNumbers().filter(Common::isPrime);
+    }
+    
+    public static boolean isPrime(int i) {
+        return i >= 2 && IntStream.rangeClosed(2, (int) Math.sqrt(i)).allMatch(e -> i % e != 0);
     }
     
     private static IntStream naturalNumbers() {
--- src/main/java/projecteuler/problems/Problem27.java
+++ src/main/java/projecteuler/problems/Problem27.java
@@ -1,10 +1,49 @@
 package projecteuler.problems;
 
+import java.util.Comparator;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
+import static projecteuler.problems.Problem21.memoized;
+
+@SuppressWarnings("ConstantConditions")
 public class Problem27 implements Problem<Integer> {
+    
+    private static Function<Integer, Boolean> IS_PRIME = memoized(Common::isPrime);
     
     @Override
     public Integer getCalculatedSolution() {
-        return null;
+        return range().boxed().flatMap(a -> range().mapToObj(b -> new Pair(a, b)))
+                .parallel()
+                .max(Comparator.comparing(Problem27::maxNumberOfPrimes))
+                .map(pair -> pair.a * pair.b)
+                .get();
+    }
+    
+    private IntStream range() {
+        return IntStream.rangeClosed(-999, 999);
+    }
+    
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    private static int maxNumberOfPrimes(Pair pair) {
+        int[] primesCount = new int[1];
+        
+        IntStream.iterate(0, i -> i + 1)
+                .map(n -> n * n + pair.a * n + pair.b)
+                .peek((i) -> primesCount[0]++)
+                .anyMatch(i -> !IS_PRIME.apply(i));
+        
+        return primesCount[0];
+    }
+    
+    
+    private static class Pair {
+        final int a, b;
+        
+        private Pair(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
     }
     
     @Override
