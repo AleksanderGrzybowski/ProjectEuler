f241965c7271aab93746dd2b8476feebecf1cb09
Aleksander Grzybowski <kelog@orange.pl>
2017-10-31 15:07:34 +0100

    28

--- src/main/java/projecteuler/problems/Problem28.java
+++ src/main/java/projecteuler/problems/Problem28.java
@@ -1,10 +1,57 @@
 package projecteuler.problems;
 
+import java.util.stream.IntStream;
+
 public class Problem28 implements Problem<Integer> {
     
     @Override
     public Integer getCalculatedSolution() {
-        return null;
+        return new Spiral(1001).diagonalSum();
+    }
+    
+    private static class Spiral {
+        
+        private int[][] spiral;
+        
+        Spiral(int size) {
+            // boudary made with '0'-s
+            spiral = new int[size + 2][size + 2];
+            
+            int currentNumber = 1;
+            int posX, posY;
+            posX = posY = size / 2 + 1;
+            
+            spiral[posX][posY] = currentNumber++;
+            posX++;
+            
+            for (int i = 0; i < size / 2; ++i) {
+                while (spiral[posX - 1][posY] != 0) {
+                    spiral[posX][posY] = currentNumber++;
+                    posY++;
+                }
+                
+                while (spiral[posX][posY - 1] != 0) {
+                    spiral[posX][posY] = currentNumber++;
+                    posX--;
+                }
+                
+                while (spiral[posX + 1][posY] != 0) {
+                    spiral[posX][posY] = currentNumber++;
+                    posY--;
+                }
+                
+                while (spiral[posX][posY + 1] != 0) {
+                    spiral[posX][posY] = currentNumber++;
+                    posX++;
+                }
+            }
+        }
+        
+        int diagonalSum() {
+            return IntStream.range(0, spiral.length).map(i -> spiral[i][i]).sum()
+                    + IntStream.range(0, spiral.length).map(i -> spiral[i][spiral.length - 1 - i]).sum()
+                    - spiral[spiral.length / 2][spiral.length / 2];
+        }
     }
     
     @Override
