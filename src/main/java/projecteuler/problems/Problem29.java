b5d9c7ff18f4e3e4a0ed3f9567f46ea450e63390
Aleksander Grzybowski <kelog@orange.pl>
2017-11-01 20:58:05 +0100

    29

--- src/main/java/projecteuler/problems/Problem29.java
+++ src/main/java/projecteuler/problems/Problem29.java
@@ -1,14 +1,24 @@
 package projecteuler.problems;
 
+import java.math.BigInteger;
+import java.util.stream.IntStream;
+
 public class Problem29 implements Problem<Integer> {
-
-	@Override
-	public Integer getCalculatedSolution() {
-return null;
-	}
-
-	@Override
-	public Integer getExpectedSolution() {
-		return 9183;
-	}
+    
+    @Override
+    public Integer getCalculatedSolution() {
+        return (int) range().mapToObj(BigInteger::valueOf)
+                .flatMap(a -> range().mapToObj(a::pow))
+                .distinct()
+                .count();
+    }
+    
+    private IntStream range() {
+        return IntStream.rangeClosed(2, 100);
+    }
+    
+    @Override
+    public Integer getExpectedSolution() {
+        return 9183;
+    }
 }
