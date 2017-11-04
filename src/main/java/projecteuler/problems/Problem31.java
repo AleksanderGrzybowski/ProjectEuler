47decbf79976f6c0532ce5789da2fb930fd3c95f
Aleksander Grzybowski <kelog@orange.pl>
2017-11-04 18:53:06 +0100

    stub 31

---
+++ src/main/java/projecteuler/problems/Problem31.java
@@ -0,0 +1,14 @@
+package projecteuler.problems;
+
+public class Problem31 implements Problem<Integer> {
+    
+    @Override
+    public Integer getCalculatedSolution() {
+        return null;
+    }
+    
+    @Override
+    public Integer getExpectedSolution() {
+        return 73682;
+    }
+}
