a85455d3909ac7cab582304bfb8a5d7c3a03d022
Aleksander Grzybowski <kelog@orange.pl>
2017-11-04 18:36:50 +0100

    30

--- src/main/java/projecteuler/problems/Problem30.java
+++ src/main/java/projecteuler/problems/Problem30.java
@@ -1,10 +1,20 @@
 package projecteuler.problems;
 
+import java.util.stream.IntStream;
+
 public class Problem30 implements Problem<Integer> {
     
     @Override
     public Integer getCalculatedSolution() {
-        return null;
+        return IntStream.range(2, 1000000).parallel()
+                .filter(Problem30::canBeWrittenAsSumOfFifthPowers)
+                .sum();
+    }
+    
+    private static boolean canBeWrittenAsSumOfFifthPowers(int number) {
+        return number == Integer.toString(number).chars()
+                .map(i -> (int) Math.pow(i - '0', 5))
+                .sum();
     }
     
     @Override
