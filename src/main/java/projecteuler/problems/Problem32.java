53db86446099328904af00c6e28b43ba36193185
Aleksander Grzybowski <kelog@orange.pl>
2017-11-05 10:56:12 +0100

    32

--- src/main/java/projecteuler/problems/Problem32.java
+++ src/main/java/projecteuler/problems/Problem32.java
@@ -1,10 +1,29 @@
 package projecteuler.problems;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toSet;
+
 public class Problem32 implements Problem<Integer> {
+    
+    private static final Set<String> PANDIGITAL_SET = Arrays.stream("123456789".split("")).collect(toSet());
+    private static final int LIMIT = 2000;
     
     @Override
     public Integer getCalculatedSolution() {
-        return null;
+        return IntStream.rangeClosed(1, LIMIT).flatMap(i ->
+                IntStream.rangeClosed(1, LIMIT)
+                        .filter(j -> isPandigital("" + i + j + (i * j)))
+                        .map(j -> i * j)
+        )
+                .distinct()
+                .sum();
+    }
+    
+    private boolean isPandigital(String number) {
+        return number.length() == 9 && Arrays.stream(number.split("")).collect(toSet()).equals(PANDIGITAL_SET);
     }
     
     @Override
