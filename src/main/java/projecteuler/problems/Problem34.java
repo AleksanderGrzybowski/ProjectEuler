0a7741de78c6c9a86c87c6afbe0a7809984e7695
Aleksander Grzybowski <kelog@orange.pl>
2017-11-07 17:05:42 +0100

    34

--- src/main/java/projecteuler/problems/Problem34.java
+++ src/main/java/projecteuler/problems/Problem34.java
@@ -1,14 +1,32 @@
 package projecteuler.problems;
 
+import java.util.stream.IntStream;
+
 public class Problem34 implements Problem<Integer> {
-
-	@Override
-	public Integer getCalculatedSolution() {
-return null;
-	}
-
-	@Override
-	public Integer getExpectedSolution() {
-		return 40730;
-	}
+    
+    private static final int MAX = 50000;
+    private static final int MIN = 3;
+    
+    @Override
+    public Integer getCalculatedSolution() {
+        return IntStream.rangeClosed(MIN, MAX)
+                .filter(Problem34::isCurious)
+                .sum();
+    }
+    
+    private static boolean isCurious(int number) {
+        return number == Integer.toString(number).chars()
+                .map(i -> i - '0')
+                .map(Problem34::factorial)
+                .sum();
+    }
+    
+    private static int factorial(int number) {
+        return IntStream.rangeClosed(1, number).reduce(1, (a, b) -> a * b);
+    }
+    
+    @Override
+    public Integer getExpectedSolution() {
+        return 40730;
+    }
 }
