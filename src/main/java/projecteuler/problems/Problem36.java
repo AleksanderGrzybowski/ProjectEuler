d8dfd44f5c1deb657045d669a4fbb0ffb70ce643
Aleksander Grzybowski <kelog@orange.pl>
2017-11-08 18:34:25 +0100

    36

--- src/main/java/projecteuler/problems/Problem36.java
+++ src/main/java/projecteuler/problems/Problem36.java
@@ -1,14 +1,28 @@
 package projecteuler.problems;
 
+import java.util.stream.IntStream;
+
 public class Problem36 implements Problem<Integer> {
-
-	@Override
-	public Integer getCalculatedSolution() {
-return null;
-	}
-
-	@Override
-	public Integer getExpectedSolution() {
-		return 872187;
-	}
+    
+    private static final int MAX = 1_000_000;
+    
+    @Override
+    public Integer getCalculatedSolution() {
+        return IntStream.rangeClosed(1, MAX)
+                .filter(Problem36::isPalindromicInBase10And2)
+                .sum();
+    }
+    
+    private static boolean isPalindromicInBase10And2(int number) {
+        return isPalindromic(Integer.toString(number)) && isPalindromic(Integer.toString(number, 2));
+    }
+    
+    private static boolean isPalindromic(String number) {
+        return number.equals(new StringBuilder(number).reverse().toString());
+    }
+    
+    @Override
+    public Integer getExpectedSolution() {
+        return 872187;
+    }
 }
