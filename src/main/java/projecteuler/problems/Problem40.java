9f993309a64375c355fa299320406ed2a77be4c8
Aleksander Grzybowski <kelog@orange.pl>
2017-11-10 19:21:21 +0100

    40

--- src/main/java/projecteuler/problems/Problem40.java
+++ src/main/java/projecteuler/problems/Problem40.java
@@ -1,10 +1,26 @@
 package projecteuler.problems;
 
+import java.util.stream.IntStream;
+
 public class Problem40 implements Problem<Integer> {
+    
+    private static final int MAX_LENGTH = 1_000_000;
+    private static final int MAX_POWER_OF_TEN = 6;
     
     @Override
     public Integer getCalculatedSolution() {
-        return null;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 1; builder.length() <= MAX_LENGTH; i++) {
+            builder.append(i);
+        }
+        
+        return powersOfTen(MAX_POWER_OF_TEN)
+                .map(i -> Integer.parseInt("" + builder.charAt(i - 1)))
+                .reduce(1, (a, b) -> a * b);
+    }
+    
+    private IntStream powersOfTen(int max) {
+        return IntStream.rangeClosed(0, max - 1).map(i -> (int) Math.pow(10, i));
     }
     
     @Override
