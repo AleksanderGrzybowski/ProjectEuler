7b38b5148ea09419481999bbebc190166572579e
Aleksander Grzybowski <kelog@orange.pl>
2017-11-13 18:21:53 +0100

    44

--- src/main/java/projecteuler/problems/Problem44.java
+++ src/main/java/projecteuler/problems/Problem44.java
@@ -1,14 +1,51 @@
 package projecteuler.problems;
 
-public class Problem44 implements Problem<Integer> {
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.LongStream;
 
-	@Override
-	public Integer getCalculatedSolution() {
-return null;
-	}
+import static java.lang.Math.abs;
+import static java.util.stream.Collectors.toSet;
 
-	@Override
-	public Integer getExpectedSolution() {
-		return 5482660;
-	}
+@SuppressWarnings("ConstantConditions")
+public class Problem44 implements Problem<Long> {
+    
+    private static final int MAX = 10000;
+    
+    @Override
+    public Long getCalculatedSolution() {
+        Set<Long> pentagonalNumbers = LongStream.rangeClosed(1, MAX)
+                .map(n -> n * (3 * n - 1) / 2)
+                .boxed()
+                .collect(toSet());
+        
+        return pentagonalNumbers.stream().flatMap(j ->
+                // since they are unique, we can check the actual values, not indexes
+                pentagonalNumbers.stream().filter(k -> !Objects.equals(j, k)).map(k -> new Pair(j, k))
+        )
+                .parallel()
+                .filter(pair -> pentagonalNumbers.contains(pair.j + pair.k) && pentagonalNumbers.contains(abs(pair.j - pair.k)))
+                .min(Comparator.comparing(Problem44::criteriaForD))
+                .map(Problem44::criteriaForD)
+                .orElseThrow(AssertionError::new);
+    }
+    
+    private static long criteriaForD(Pair pair) {
+        return abs(pair.j - pair.k);
+    }
+    
+    @Override
+    public Long getExpectedSolution() {
+        return 5482660L;
+    }
+    
+    private static class Pair {
+        final long j, k;
+        
+        private Pair(long j, long k) {
+            this.j = j;
+            this.k = k;
+        }
+    }
 }
