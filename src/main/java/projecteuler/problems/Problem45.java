bf255f45ee223bfd6b5799fe30a431574b6ee504
Aleksander Grzybowski <kelog@orange.pl>
2017-11-16 14:23:00 +0100

    45

--- src/main/java/projecteuler/problems/Problem45.java
+++ src/main/java/projecteuler/problems/Problem45.java
@@ -1,14 +1,59 @@
 package projecteuler.problems;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+@SuppressWarnings("ConstantConditions")
 public class Problem45 implements Problem<Long> {
-
-	@Override
-	public Long getCalculatedSolution() {
-return null;
-	}
-
-	@Override
-	public Long getExpectedSolution() {
-		return 1533776805L;
-	}
+    
+    private static final Function<Long, Long> TRIANGLE = n -> n * (n + 1) / 2;
+    private static final Function<Long, Long> PENTAGONAL = n -> n * (3 * n - 1) / 2;
+    private static final Function<Long, Long> HEXAGONAL = n -> n * (2 * n - 1);
+    private static final int EXPECTED_COMMON_ELEMENTS_SIZE = 3;
+    
+    @Override
+    public Long getCalculatedSolution() {
+        return LongStream.iterate(10, max -> max * 10).boxed().flatMap(max -> {
+            Set<Long> commonElements = intersection(
+                    generateUpTo(TRIANGLE, max),
+                    generateUpTo(PENTAGONAL, max),
+                    generateUpTo(HEXAGONAL, max)
+            );
+            
+            if (commonElements.size() == EXPECTED_COMMON_ELEMENTS_SIZE) {
+                return Stream.of(commonElements.stream().max(Long::compareTo).get());
+            } else {
+                return Stream.empty();
+            }
+        }).findFirst().get();
+    }
+    
+    @SafeVarargs
+    private static Set<Long> intersection(Set<Long> first, Set<Long>... others) {
+        Set<Long> intersection = new HashSet<>(first);
+    
+        Arrays.stream(others).forEach(intersection::retainAll);
+        
+        return intersection;
+    }
+    
+    private static Set<Long> generateUpTo(Function<Long, Long> generator, long max) {
+        Set<Long> elements = new HashSet<>();
+        
+        for (long currentIndex = 1, currentElement = Long.MIN_VALUE; currentElement <= max; currentIndex++) {
+            currentElement = generator.apply(currentIndex);
+            elements.add(currentElement);
+        }
+        
+        return elements;
+    }
+    
+    @Override
+    public Long getExpectedSolution() {
+        return 1533776805L;
+    }
 }
