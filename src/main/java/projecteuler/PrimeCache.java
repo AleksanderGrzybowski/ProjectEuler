c6b89dbf214ab634fde99f1f8c80a677f2ff656e
Aleksander Grzybowski <kelog@orange.pl>
2017-11-17 17:08:39 +0100

    47 (good old Eratostenes)

---
+++ src/main/java/projecteuler/PrimeCache.java
@@ -0,0 +1,80 @@
+package projecteuler;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.IntStream;
+
+public class PrimeCache {
+    
+    public static final PrimeCache INSTANCE = new PrimeCache();
+    
+    private int size = 100;
+    private final ConcurrentHashMap<Integer, Boolean> PRIMES = new ConcurrentHashMap<>();
+    
+    private int[] sieve;
+    
+    private PrimeCache() {
+        size = 100;
+        initialize();
+    }
+    
+    private void initialize() {
+        sieve = new int[size];
+        
+        for (int i = 2; i < sieve.length; ++i) {
+            sieve[i] = i;
+        }
+        
+        for (int i = 2; i < sieve.length; ++i) {
+            if (sieve[i] != 0) {
+                for (int j = i * 2; j < sieve.length; j += i) {
+                    sieve[j] = 0;
+                }
+            }
+        }
+    }
+    
+    private boolean isPrime(int number) {
+        ensureCapacity(number);
+        
+        return sieve[number] != 0;
+    }
+    
+    private void ensureCapacity(long number) {
+        if (number >= size) {
+            while (number >= size) {
+                size *= 10;
+            }
+            initialize();
+        }
+    }
+    
+    IntStream primes() {
+        return IntStream.iterate(2, i -> i + 1).filter(this::isPrime);
+    }
+    
+    public List<Integer> factorize(long number) {
+        List<Integer> factors = new ArrayList<>();
+        
+        while (number != 1) {
+            int factor = smallestPrimeFactorOf(number);
+            factors.add(factor);
+            number /= factor;
+        }
+        
+        return factors;
+    }
+    
+    private int smallestPrimeFactorOf(long number) {
+        ensureCapacity(number);
+        
+        for (int i = 2; i <= number; ++i) {
+            if (sieve[i] != 0 && number % i == 0) {
+                return i;
+            }
+        }
+        
+        throw new AssertionError();
+    }
+}
--- src/main/java/projecteuler/problems/Problem47.java
+++ src/main/java/projecteuler/problems/Problem47.java
@@ -1,10 +1,23 @@
 package projecteuler.problems;
 
+import projecteuler.PrimeCache;
+
+import java.util.HashSet;
+import java.util.stream.Stream;
+
+@SuppressWarnings("ConstantConditions")
 public class Problem47 implements Problem<Integer> {
-    
+
     @Override
     public Integer getCalculatedSolution() {
-        return null;
+        return Common.naturalNumbers()
+                .filter(j ->
+                        Stream.of(j, j + 1, j + 2, j + 3).allMatch(this::has4DistinctPrimeFactors)
+                ).findFirst().getAsInt();
+    }
+    
+    private boolean has4DistinctPrimeFactors(Integer i) {
+        return new HashSet<>(PrimeCache.INSTANCE.factorize(i)).size() == 4;
     }
     
     @Override
