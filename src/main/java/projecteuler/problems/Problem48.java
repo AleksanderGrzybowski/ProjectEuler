a3b742271364814903d5a441be39b6235e472945
Aleksander Grzybowski <kelog@orange.pl>
2017-11-17 19:10:12 +0100

    48

--- src/main/java/projecteuler/problems/Problem48.java
+++ src/main/java/projecteuler/problems/Problem48.java
@@ -1,14 +1,27 @@
 package projecteuler.problems;
 
+import java.math.BigInteger;
+import java.util.stream.IntStream;
+
+@SuppressWarnings("ConstantConditions")
 public class Problem48 implements Problem<String> {
-
-	@Override
-	public String getCalculatedSolution() {
-return null;
-	}
-
-	@Override
-	public String getExpectedSolution() {
-		return "9110846700";
-	}
+    
+    private static final IntStream RANGE = IntStream.range(1, 1000);
+    private static final int LAST_DIGITS = 10;
+    
+    @Override
+    public String getCalculatedSolution() {
+        String sum = RANGE
+                .mapToObj(i -> BigInteger.valueOf(i).pow(i))
+                .reduce(BigInteger::add)
+                .get()
+                .toString();
+        
+        return sum.substring(sum.length() - LAST_DIGITS);
+    }
+    
+    @Override
+    public String getExpectedSolution() {
+        return "9110846700";
+    }
 }
